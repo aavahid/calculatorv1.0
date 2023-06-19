3e73a1d41287e37073a9f27225b50f4d4b0ae6bd
vahidaa <vahidaa@code.edu.az>
2023-06-19 11:23:55 +0400

    some updates

--- calculator/Program.cs
+++ calculator/Program.cs
@@ -1,17 +1,17 @@
 ﻿using System;
 
-Console.WriteLine("Xos gelmisiniz");
+Console.WriteLine("Calculator v1.0'a xos gelmisiniz");
 bool isContinue = true;
 
 while (isContinue == true)
 {
-    Console.WriteLine("             ");
+    Console.WriteLine(" ");
     Console.WriteLine("1. Toplama");
     Console.WriteLine("2. Cixma");
     Console.WriteLine("3. Vurma");
     Console.WriteLine("4. Bolme");
     Console.WriteLine("5. Proqramdan cix");
-    Console.WriteLine("              ");
+    Console.WriteLine(" ");
     Console.Write("Seciminizi edin: ");
 
     string command = Console.ReadLine();
@@ -20,38 +20,37 @@ while (isContinue == true)
     {
         case "1":
             Console.Write("1. ededi daxil edin: ");
-            int toplayan = Convert.ToInt32(Console.ReadLine());
+            int toplanan1 = Convert.ToInt32(Console.ReadLine());
             Console.Write("2. ededi daxil edin: ");
-            int toplanan = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("  ");
+            int toplanan2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(" ");
 
-            Console.WriteLine("Netice: " + (toplayan + toplanan));
+            Console.WriteLine("Netice: " +toplanan1+ " + " +toplanan2+ " = " +(toplanan1 + toplanan2));
             break;
         case "2":
             Console.Write("1. ededi daxil edin: ");
-            int cixilan = Convert.ToInt32(Console.ReadLine());
+            int azalan = Convert.ToInt32(Console.ReadLine());
             Console.Write("2. ededi daxil edin: ");
-            int cixan = Convert.ToInt32(Console.ReadLine());
+            int cixilan = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("  ");
-            Console.WriteLine("Netice: " + (cixilan - cixan));
+            Console.WriteLine("Netice: " +azalan+ " - " +cixilan+ " = " +(azalan - cixilan));
             break;
         case "3":
             Console.Write("1. ededi daxil edin: ");
-            int vurulan = Convert.ToInt32(Console.ReadLine());
+            int vuruq1 = Convert.ToInt32(Console.ReadLine());
             Console.Write("2. ededi daxil edin: ");
-            int vuran = Convert.ToInt32(Console.ReadLine());
+            int vuruq2 = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("  ");
-
-            Console.WriteLine("Netice: " + (vurulan * vuran));
+            Console.WriteLine("Netice: " +vuruq1+ " * " +vuruq2+ " = " +(vuruq1 * vuruq2));
+     
             break;
         case "4":
-            Console.Write("1. ededi daxil edin: ");
+            Console.Write("1. ededi daxil edin: " );
             int bolunen = Convert.ToInt32(Console.ReadLine());
             Console.Write("2. ededi daxil edin: ");
             int bolen = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("  ");
-
-            Console.WriteLine("Netice: " + (bolunen / bolen));
+            Console.WriteLine("Netice: " +bolunen+ " / " +bolen+ " = " +(bolunen / bolen));
             break;
         case "5":
             Console.WriteLine("Sagolun. Proqramdan cixis edilir");
@@ -59,7 +58,7 @@ while (isContinue == true)
             break;
         default:
             Console.WriteLine(" ");
-            Console.WriteLine("  ");
+            Console.WriteLine(" ");
 
             Console.WriteLine("Sehv secim etdiniz bir daha secim edin");
             break;
